4dd3d3b8abadf596790798d60038c478407ea03e
rulongwang <rulong.wang@gmail.com>
2013-02-25 23:17:42 +0800

    Initial Commit

---
+++ CMframe/FMAppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  FMAppDelegate.h
+//  CMframe
+//
+//  Created by rulong on 2/25/13.
+//  Copyright (c) 2013 rulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ CMframe/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CMframe
+//
+//  Created by rulong on 2/25/13.
+//  Copyright (c) 2013 rulong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FMAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FMAppDelegate class]));
+    }
+}
